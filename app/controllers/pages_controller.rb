e6fc8f6cef7c14fe7a3f4ead7ef66bb148f984fe
ashwin <ashwin.ashwinkhanna@gmail.com>
2019-02-09 22:51:37 -0500

    Home page of toffee with boilerplate

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+
+  # All our controllers, which we will create, are going to
+  # inherit from ApplicationController class. Which means that
+  # all methods defined inside this class are going to be
+  # available across all our controllers.
+
+  def index
+  end
+end
