564f7512919eabe98bb93c4d2348e4e7072cf301
ashwin <ashwin.ashwinkhanna@gmail.com>
2019-02-19 09:01:29 -0500

    Added posts and categories factories

---
+++ spec/factories/categories.rb
@@ -0,0 +1,7 @@
+#Define test data
+FactoryGirl.define do
+  factory :category do
+    sequence(:name) { |n| "name#{n}" }
+    sequence(:branch) { |n| "branch#{n}" }
+  end
+end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,9 @@
+#Define test data
+FactoryGirl.define do
+  factory :post do
+    title 'a' * 20
+    content 'a' * 20
+    user
+    category
+  end
+end
