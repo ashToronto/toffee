579a89073bd917e94ba07d534b648d9e43e4e82b
ashwin <ashwin.ashwinkhanna@gmail.com>
2019-02-28 12:31:17 -0500

    Create a UI to create new posts - Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
+  
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
   end
 
   def hobby
@@ -16,20 +33,11 @@ class PostsController < ApplicationController
   end
 
   def get_posts
-    branch = params[:action]
-    search = params[:search]
-    category = params[:category]
-
-    if category.blank? && search.blank?
-        posts = Post.by_branch(branch).all
-      elsif category.blank? && search.present?
-        posts = Post.by_branch(branch).search(search)
-      elsif category.present? && search.blank?
-        posts = Post.by_category(branch, category)
-      elsif category.present? && search.present?
-        posts = Post.by_category(branch, category).search(search)
-      else
-    end
+    PostsForBranchService.new({
+      search: params[:search],
+      category: params[:category],
+      branch: params[:action]
+    }).call
   end
 
   private
@@ -41,4 +49,10 @@ class PostsController < ApplicationController
       format.js { render partial: 'posts/posts_pagination_page' }
     end
   end
+
+  def post_params
+  params.require(:post).permit(:content, :title, :category_id)
+                       .merge(user_id: current_user.id)
+  end
+
 end
